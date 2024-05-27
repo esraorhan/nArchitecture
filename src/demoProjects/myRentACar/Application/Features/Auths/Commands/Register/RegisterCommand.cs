dfaff488accc6004031d4b5ee4f4ae01d5a6091c
esra orhan <esraorhan333@hotmail.com>
2024-05-27 10:14:09 +0300

    5.Gün  JWT altyapısı

---
+++ src/demoProjects/myRentACar/Application/Features/Auths/Commands/Register/RegisterCommand.cs
@@ -0,0 +1,30 @@
+﻿using Application.Features.Auths.Dtos;
+using Application.Features.Auths.Rules;
+using Application.Services.Repositories;
+using Core.Security.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Auths.Commands.Register
+{
+    public class RegisterCommand :IRequest<RegisteredDto>
+    {
+        public UserForRegisterDto UserForRegisterDto { get; set; }
+        public string IpAddress {  get; set; }
+        public class RegisterCommandHandler : IRequestHandler<RegisterCommand, RegisteredDto>
+        {
+            private readonly AuthBusinessRules _businessRules;
+            private readonly IUserRepository _userRepository;
+            public Task<RegisteredDto> Handle(RegisterCommand request, CancellationToken cancellationToken)
+            {
+                //token oluşturma kodlarını buraya yazılablir fakat login de gerektiği için generate etmek gerekir.
+                //bu yüzden servis yaklaşımı yapılacak.Service klasöründe =>
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
