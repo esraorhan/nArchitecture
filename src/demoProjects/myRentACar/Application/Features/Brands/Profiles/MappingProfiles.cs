e9d5a03c2c3de70f60c0dcf9cb66ca5a3e26366c
esraorhan <esraorhan333@gmail.com>
2024-05-31 00:52:10 +0300

    test

--- src/demoProjects/myRentACar/Application/Features/Brands/Profiles/MappingProfiles.cs
+++ src/demoProjects/myRentACar/Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -18,6 +18,7 @@ namespace Application.Features.Brands.Profiles
         {
             CreateMap<Brand,CreatedBrandDto>().ReverseMap();
             CreateMap<Brand, CreateBrandCommand>().ReverseMap();
+
             CreateMap<IPaginate<Brand>,BrandListModel>().ReverseMap();
             CreateMap<Brand,BrandListDto>().ReverseMap();
             CreateMap<Brand, BrandGetByIdDto>().ReverseMap();
